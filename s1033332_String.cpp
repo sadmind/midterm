bda451317687745e78234b05082e4d376e33cdc4
asdcxz222 <asdcxz222>
2015-05-02 18:26:16 +0800

    1

--- s1033332_String.cpp
+++ s1033332_String.cpp
@@ -7,6 +7,7 @@ int main()
 {
 	cout << "A = str1: to be" << endl << "B = X = str2: the = the" << endl << "C = str3 : or not to be = or not to be" << endl << "D = A + C : to be or not to be" << endl;
 	cout << "D = A + str3 : to be or not to be" << endl << "D = str1 + C : to be or not to be"<<endl;
+	cout << "-------------------------------"<<endl;
 	system("pause");
 	return 0;
 }
