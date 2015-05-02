9c28d335ba26e835217d756fc3d93c0c5ff8af64
s1031430 <w5151381guy@gmail.com>
2015-05-02 18:06:51 +0800

    midterm

--- s1031430_midterm/s1031430_string.h
+++ s1031430_midterm/s1031430_string.h
@@ -2,9 +2,9 @@
 #define PG2String_H
 using namespace std;
 
-class String 
+class String
 {
-	friend ostream &operator<<(ostream& output, const String& str);
+	friend std::ostream &operator<<(std::ostream& output, const String& str);
 	// Inserts the sequence of characters that conforms value of str into os.
 	// This function overloads operator<< to behave as described 
 	// in ostream::operator<< for c-strings, but applied to string objects.
@@ -25,13 +25,13 @@ public:
 
 	String(); // Constructs an empty string, with a length of zero characters. ***default capacity = 10
 
-	String( const String &str ); // Constructs a copy of "str". (copy constructor)
+	String(const String &str); // Constructs a copy of "str". (copy constructor)
 
-	String(const String& str, size_t pos, size_t len = npos); // (substring constructor)
+	String(const String& str, size_t , size_t ); // (substring constructor)
 	// Copies the portion of str that begins at the character position "pos" and spans "len" characters 
 	// (or until the end of str, if either str is too short or if len is string::npos).
 
-	String( const char *s ); 
+	String(const char *s);
 	// Copies the array of character (C-string) pointed by s.
 	// The char *s is a pointer to a null-terminated sequence of characters. (all following char* are the same/)
 	// The sequence is copied as the new value for the string.
@@ -40,14 +40,14 @@ public:
 
 	char* begin(); // Returns an pointer referring to the first character in the string.
 
-	char* end(); 
+	char* end();
 	// Returns an pointer referring to the past-the-end character of the string.
 	// The past-the-end character is a theoretical character that would follow the last character in the string.
 	// It shall not be dereferenced.
 
 	size_t getSize() const; // Returns the number of characters in the string.
 
-	void resize( size_t n ); 
+	void resize(size_t n);
 	// Resizes the string to a length of n characters.
 	// If n is smaller than the current string length,
 	// the current value is shortened to its first n character,
@@ -57,17 +57,17 @@ public:
 	// as many characters as needed to reach a size of n.
 	// The new elements are initialized as copies of null characters ('\0').
 
-	void resize( size_t n, char c ); 
+	void resize(size_t n, char c);
 	// Similar to above one. The new elements are initialized as copies of c.
 
-	size_t getCapacity() const; 
+	size_t getCapacity() const;
 	// NOTE: default capacity = 10
 	// Returns the size of the storage space currently allocated for the string,
 	// expressed in terms of elements.
 	// *This capacity is not necessarily equal to the string length. It can be equal or greater, 
 	// with the extra space allowing the object to optimize its operations when new characters are added to the string.
 
-	void reserve( size_t n = 0 ); // Requests that the string capacity be enough to contain n characters.
+	void reserve(size_t); // Requests that the string capacity be enough to contain n characters.
 	// If n is greater than the current string capacity,
 	// the function causes the container to reallocate its storage increasing its capacity to n.
 	// In all other cases, the function call does NOT cause a reallocation and
@@ -75,62 +75,62 @@ public:
 
 	void shrink_to_fit(); // Requests the container to reduce its capacity to fit its size.
 
-	void clear(); 
+	void clear();
 	// Erases the contents of the string, which becomes an empty string 
 	// (with a length of 0 characters, the Capacity is not affected).
 
 	bool empty() const; // Returns whether the string is empty (i.e. whether its size is 0).
 
-	char& front(); 
+	char& front();
 	// *Returns a reference to the first character of the string.
 	// This function shall not be called on empty strings.
 
-	char& back(); 
+	char& back();
 	// *Returns a reference to the last character of the string.
 	// This function shall not be called on empty strings.
 
-	void push_back( char c ); 
+	void push_back(char c);
 	// Appends character c to the end of the string, increasing its length by one.
 	// *Return the reference of itself
 
-	void pop_back(); 
+	void pop_back();
 	// Erases the last character of the string, effectively reducing its size by one. 
 	// (the string capacity is not affected)
 	// *Return the reference of itself
 
-	String& append( const String &str ); 
+	String& append(const String &str);
 	// Appends a str on the tail of current String. 
 	// *Return the reference of itself
 
-	String& substr(size_t pos = 0, size_t len = npos) const;
+	String& substr(size_t, size_t) const;
 	// The substring is the portion of the object that starts at character position pos and spans len characters 
 	// (or until the end of the string, whichever comes first).
 	// *Returns a newly constructed string object with its value initialized to a copy of a substring of this object.
 
-	String& insert( size_t pos, const String &str);
+	String& insert(size_t pos, const String &str);
 	// Inserts a copy of a str at "pos".
 	// example: str = "to be question", str2 = "the "
 	// str.insert(6,str2); // = to be (the )question
 	// *Return the reference of itself
 
-	String& insert( size_t pos, const String &str, size_t subpos, size_t sublen );
+	String& insert(size_t pos, const String &str, size_t subpos, size_t sublen);
 	// Inserts a copy of a substring of str at "pos". The substring is the portion of "str" that
 	// begins at the character position subpos and spans sublen characters (or until the end of str).
 	// example: str = "to be the question", str3 = "or not to be"
 	// str.insert(6,str3,3,4); // to be (not )the question
 	// *Return the reference of itself
 
-	String& erase( size_t subpos, size_t sublen ); 
+	String& erase(size_t subpos, size_t sublen);
 	// Erases the portion of the string value that begins at the character position pos and spans len characters 
 	// (or until the end of the string, if either the content is too short or if len is string::npos.)
 	// *Return the reference of itself
 
-	size_t find( const String &str, size_t pos = 0 ) const;
+	size_t find(const String &str, size_t) const;
 	// Searches the portion of the string value that begins at the character position pos until the end of the string
 	// for the first occurrence of the string str.
 	// Return Value: The position of the first character of the first match. If no matches were found, the function returns String::npos.
 
-	size_t find_first_of( const String str, size_t pos = 0 ) const;
+	size_t find_first_of(const String str, size_t pos = 0) const;
 	// Searches the portion of the string value that begins at the character position "pos" until the end of the string
 	// for the first character that matches *any* of the characters of the string str.
 	// Return Value: The position of the first character that matches. If no matches are found, the function returns String::npos.
@@ -161,7 +161,7 @@ private:
 	size_t capacity; // the size of the storage space currently allocated for the string, expressed in terms of elements.
 	// This capacity is not necessarily equal to the string size. It can be equal or greater,
 	// with the extra space allowing to accommodate for growth without the need to reallocate on each insertion.
-	char *sPtr; // points to a dynamically allocated array which is used to store the elements of the string
+	char *sPtr[100000]; // points to a dynamically allocated array which is used to store the elements of the string
 }; // end class String
 
 #endif
