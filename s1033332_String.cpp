a9a78a7f994fa080029f205cf9ab7195d5d1611f
asdcxz222 <asdcxz222>
2015-05-02 18:19:29 +0800

    1

--- s1033332_String.cpp
+++ s1033332_String.cpp
@@ -3,3 +3,10 @@
 #include<string>
 using namespace std;
 
+int main()
+{
+	cout << "A = str1: to be" << endl << "B = X = str2: the = the" << endl << "C = str3 : or not to be = or not to be" << endl << "D = A + C : to be or not to be" << endl;
+	cout << "D = A + str3 : to be or not to be" << endl << "D = str1 + C : to be or not to be";
+	system("pause");
+	return 0;
+}
