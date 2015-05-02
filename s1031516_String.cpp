f22a73f3f2316e0313fffb8361a5bac034167b6f
fanguts <fang850208@gmail.com.tw>
2015-05-02 18:17:57 +0800

    String

---
+++ s1031516_String.cpp
@@ -0,0 +1,196 @@
+#include "s1031516_String.h"
+#include <string>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+
+String::String()
+{
+	char *a;
+	cin >> *a;
+	a[10] = { };
+}
+
+String::String(const String &str)
+{
+	int n = sizeof(str);
+	string f[100];
+	for (int i = 0; i <= sizeof(&str); i++)
+		f[i] = str[i];
+}
+
+String::String(const String& str, size_t pos, size_t len = npos)
+{
+
+}
+
+String::String(const char *s)
+{
+
+}
+
+
+
+String::~String()
+{
+	*this = {};
+}
+
+char* String::begin()
+{
+	String f[1] = {};
+
+}
+
+char* String::end()
+{
+
+}
+
+size_t String::getSize() const
+{
+	int count;
+	count = sizeof(*this) - 1;
+	return count;
+}
+
+void String::resize(size_t n)
+{
+	
+}
+
+void String::resize(size_t n, char c)
+{
+
+}
+
+size_t String::getCapacity() const
+{
+	int a;
+	a = sizeof(*this);
+	return a;
+}
+
+void String::reserve(size_t n = 0)
+{
+}
+
+void String::shrink_to_fit()
+{
+
+}
+
+void String::clear()
+{
+}
+
+bool String::empty() const
+{
+	return false;
+}
+
+char &String::front()
+{
+	String a[100] = { *this };
+
+	if (empty() != true)
+	{
+		char s;
+		return s;
+	}
+
+}
+
+
+char &String::back()
+{
+
+}
+
+void String::push_back(char c)
+{
+
+}
+
+void String::pop_back()
+{
+
+}
+
+String &String::append(const String &str)
+{
+	
+}
+
+String &String::substr(size_t pos = 0, size_t len = npos) const
+{
+
+}
+
+String &String::insert(size_t pos, const String &str)
+{
+
+}
+
+String &String::insert(size_t pos, const String &str, size_t subpos, size_t sublen)
+{
+
+}
+
+String &String::erase(size_t subpos, size_t sublen)
+{
+
+}
+
+
+
+char &String::operator[] (size_t pos)
+{
+	String a[100] = { *this };
+}
+
+const char &String::operator[] (size_t pos) const
+{
+
+}
+
+String String::operator+ (const String& rhs)
+{
+	String a = *this;
+	int b = sizeof(rhs);
+	string c[100];
+	for (int i = 0; i <= 100; i++)
+		c[i] = a[i] + rhs[i];
+}
+
+String String:: operator+ (const char*   rhs)
+{
+
+}
+
+String &String::operator= (const String& str)
+{
+
+}
+
+String &String::operator= (const char* s)
+{
+
+}
+
+bool String::operator==(const String &str)
+{
+
+}
+
+bool String::operator!=(const String &str)
+{
+
+}
+
+ostream &operator<<(ostream& output, const String& str)
+{
+	
+}
